46a9ff0627974111f1e5266f5909498ea2724e20
kova237 <dkova07@gmail.com>
2016-10-21 17:10:54 +0200

    login
    mogućnost login-a u aplikaciju

---
+++ azil.Designer.vb
@@ -0,0 +1,120 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
+Partial Class Form1
+    Inherits System.Windows.Forms.Form
+
+    'Form overrides dispose to clean up the component list.
+    <System.Diagnostics.DebuggerNonUserCode()> _
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Required by the Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'NOTE: The following procedure is required by the Windows Form Designer
+    'It can be modified using the Windows Form Designer.  
+    'Do not modify it using the code editor.
+    <System.Diagnostics.DebuggerStepThrough()> _
+    Private Sub InitializeComponent()
+        Me.btnLogin = New System.Windows.Forms.Button()
+        Me.btnIzlaz = New System.Windows.Forms.Button()
+        Me.Label1 = New System.Windows.Forms.Label()
+        Me.Label2 = New System.Windows.Forms.Label()
+        Me.Label3 = New System.Windows.Forms.Label()
+        Me.txtUsername = New System.Windows.Forms.TextBox()
+        Me.txtPassword = New System.Windows.Forms.TextBox()
+        Me.SuspendLayout()
+        '
+        'btnLogin
+        '
+        Me.btnLogin.Location = New System.Drawing.Point(217, 187)
+        Me.btnLogin.Name = "btnLogin"
+        Me.btnLogin.Size = New System.Drawing.Size(75, 23)
+        Me.btnLogin.TabIndex = 0
+        Me.btnLogin.Text = "Login"
+        Me.btnLogin.UseVisualStyleBackColor = True
+        '
+        'btnIzlaz
+        '
+        Me.btnIzlaz.Location = New System.Drawing.Point(323, 187)
+        Me.btnIzlaz.Name = "btnIzlaz"
+        Me.btnIzlaz.Size = New System.Drawing.Size(75, 23)
+        Me.btnIzlaz.TabIndex = 1
+        Me.btnIzlaz.Text = "Izlaz"
+        Me.btnIzlaz.UseVisualStyleBackColor = True
+        '
+        'Label1
+        '
+        Me.Label1.AutoSize = True
+        Me.Label1.Location = New System.Drawing.Point(231, 24)
+        Me.Label1.Name = "Label1"
+        Me.Label1.Size = New System.Drawing.Size(154, 13)
+        Me.Label1.TabIndex = 2
+        Me.Label1.Text = "Unesite korisničko ime i lozinku"
+        '
+        'Label2
+        '
+        Me.Label2.AutoSize = True
+        Me.Label2.Location = New System.Drawing.Point(136, 96)
+        Me.Label2.Name = "Label2"
+        Me.Label2.Size = New System.Drawing.Size(75, 13)
+        Me.Label2.TabIndex = 3
+        Me.Label2.Text = "Korisničko ime"
+        '
+        'Label3
+        '
+        Me.Label3.AutoSize = True
+        Me.Label3.Location = New System.Drawing.Point(149, 129)
+        Me.Label3.Name = "Label3"
+        Me.Label3.Size = New System.Drawing.Size(44, 13)
+        Me.Label3.TabIndex = 4
+        Me.Label3.Text = "Lozinka"
+        '
+        'txtUsername
+        '
+        Me.txtUsername.Location = New System.Drawing.Point(217, 89)
+        Me.txtUsername.Name = "txtUsername"
+        Me.txtUsername.Size = New System.Drawing.Size(181, 20)
+        Me.txtUsername.TabIndex = 5
+        '
+        'txtPassword
+        '
+        Me.txtPassword.Location = New System.Drawing.Point(217, 122)
+        Me.txtPassword.Name = "txtPassword"
+        Me.txtPassword.PasswordChar = Global.Microsoft.VisualBasic.ChrW(42)
+        Me.txtPassword.Size = New System.Drawing.Size(181, 20)
+        Me.txtPassword.TabIndex = 6
+        '
+        'Form1
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.ClientSize = New System.Drawing.Size(639, 261)
+        Me.Controls.Add(Me.txtPassword)
+        Me.Controls.Add(Me.txtUsername)
+        Me.Controls.Add(Me.Label3)
+        Me.Controls.Add(Me.Label2)
+        Me.Controls.Add(Me.Label1)
+        Me.Controls.Add(Me.btnIzlaz)
+        Me.Controls.Add(Me.btnLogin)
+        Me.Name = "Form1"
+        Me.Text = "Login"
+        Me.ResumeLayout(False)
+        Me.PerformLayout()
+
+    End Sub
+    Friend WithEvents btnLogin As System.Windows.Forms.Button
+    Friend WithEvents btnIzlaz As System.Windows.Forms.Button
+    Friend WithEvents Label1 As System.Windows.Forms.Label
+    Friend WithEvents Label2 As System.Windows.Forms.Label
+    Friend WithEvents Label3 As System.Windows.Forms.Label
+    Friend WithEvents txtUsername As System.Windows.Forms.TextBox
+    Friend WithEvents txtPassword As System.Windows.Forms.TextBox
+
+End Class
---
+++ azil.vb
@@ -0,0 +1,54 @@
+﻿Imports System.Data.SqlClient
+Imports System.Data
+
+
+Public Class Form1
+
+    Private da As SqlDataAdapter
+    Private conn As New SqlConnection
+    Private conStr As String = "Data Source=31.147.204.119\PISERVER,1433;Initial Catalog=16014_DB;User ID=16014_User; Password=eY4PSzGY"
+    Private cmd As SqlCommand
+    Private sql As String
+
+
+    Private Sub connection()
+
+        If conn.State = ConnectionState.Closed Then
+            conn.ConnectionString = conStr
+            conn.Open()
+        End If
+    End Sub
+
+    Private Sub Form1_Load(sender As Object, e As EventArgs) Handles MyBase.Load
+        
+    End Sub
+
+    Private Sub btnLogin_Click(sender As Object, e As EventArgs) Handles btnLogin.Click
+        connection()
+        sql = "Select * from login Where korisničko_ime= '" & txtUsername.Text & "' And lozinka='" & txtPassword.Text & "' "
+        da = New SqlDataAdapter(sql, conn)
+        Dim ds As New DataSet
+        da.Fill(ds, "login")
+        If txtUsername.Text = "" Then
+            MsgBox("Unesite korisničko ime")
+        ElseIf txtPassword.Text = "" Then
+            MsgBox("Unesite lozinku")
+        Else
+            If ds.Tables("login").Rows.Count <> 0 Then
+
+                Form2.Show()
+                Me.Hide()
+            Else
+                MsgBox("Neuspješno logiranje")
+            End If
+            
+            End If
+            conn.Close()
+
+    End Sub
+
+    Private Sub btnIzlaz_Click(sender As Object, e As EventArgs) Handles btnIzlaz.Click
+        Me.Close()
+
+    End Sub
+End Class
